5f03ec13b945c3c1d9659b969df1c36802f95487
Geslain Dahan <geslain.dahan@keyrus.com>
2019-04-24 19:34:26 +0200

    Add react text selection popover

--- src/components/AskSlack/index.js
+++ src/components/AskSlack/index.js
@@ -1,4 +1,4 @@
 import React from "react"
 import { FaSlack } from "react-icons/fa"
 
-export default () => <span><a href="slack://channel?team=TCH0UKXBQ&id=GH84LUPV5"><FaSlack /></a></span>
+export default () => <span><a href="slack://channel?team=TCH0UKXBQ&id=GH84LUPV5" target="_blank"><FaSlack /></a></span>
--- src/index.js
+++ src/index.js
@@ -2,16 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import selectionSharer from './selectionSharer'
-
 // CSS
 import './index.css';
-import 'share-this/dist/share-this.css'
 
 const container  = document.createElement('div')
 container.id = "root"
 document.getElementsByTagName('body')[0].appendChild(container)
-
-selectionSharer.init();
 
 ReactDOM.render(<App />, document.getElementById('root'));
