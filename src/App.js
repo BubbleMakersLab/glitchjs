9fb9e4735a68487f301bf6ba130ffe6daae6ee6b
Jean-Marc Martins <Jean-marc.Martins@keyruscorp.key>
2019-04-25 10:29:48 +0200

    Add dialog for raise yo hand feature

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import ModuleProgress from "./components/ModuleProgress";
 import SelectionPopover from "./components/SelectionPopover";
+import ChatDialog from "./components/RaiseYoHand"
 
 function App() {
     return (
         <div className="App">
             <ModuleProgress/>
             <SelectionPopover/>
+            <ChatDialog />
         </div>
     );
 }
--- src/components/RaiseYoHand/index.js
+++ src/components/RaiseYoHand/index.js
@@ -1,4 +1,67 @@
 import React from "react"
 import { FaHandsHelping } from "react-icons/fa"
 
-export default () => <span><FaHandsHelping /></span>
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import FilledInput from '@material-ui/core/FilledInput';
+
+
+class ChatDialog extends React.Component {
+
+    state = {
+        open: false,
+    };
+
+    handleClickOpen = () => {
+        this.setState({ open: true });
+    };
+
+    handleClose = () => {
+        this.setState({ open: false });
+    };
+
+    render() {
+        const { classes, onClose, selectedValue, ...other } = this.props
+
+        return (
+            <div>
+                <Button variant="outlined" color="primary" onClick={this.handleClickOpen}>
+                    <FaHandsHelping />
+                </Button>
+                <Dialog
+                    open={this.state.open}
+                    onClose={this.handleClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description">
+                    <DialogTitle id="alert-dialog-title">Demander de l'aide sur Slack</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                            Le prof ou d'autres étudiants te répondront directement.
+                        </DialogContentText>
+                        <FilledInput
+                            id="filled-full-width"
+                            label="Label"
+                            style={{ margin: 8 }}
+                            placeholder="Écris ton message ici."
+                            value={`"Bonjour à tous, j'ai une question concernant le passage suivant : ${window.getSelection()}, merci`}
+                            fullWidth
+                            variant="filled"
+                        />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.handleClose} color="primary" autoFocus>
+                            Poster le message
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+        )
+    }
+}
+
+
+export default () => <ChatDialog />
